c83bea7d643742a121a0530ecbee0ac54e4aae84
gourav sharma <gouravbhardwajkarnal@outlook.com>
2021-12-13 11:45:55 +0530

    Add project files.

---
+++ IPhoneRepairAPI/Controllers/ContactController.cs
@@ -0,0 +1,66 @@
+﻿using Dapper;
+using IPhoneRepairAPI.Models;
+using IPhoneRepairAPI.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IPhoneRepairAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContactController : ControllerBase
+    {
+        private readonly IDapper _dapper;
+        public ContactController(IDapper dapper)
+        {
+            _dapper = dapper;
+        }
+        [HttpPost(nameof(Create))]
+        public async Task<int> Create(Quote_Contact data)
+        {
+            var dbparams = new DynamicParameters();
+            dbparams.Add("Id", data.Id, DbType.Int32);
+            var result = await Task.FromResult(_dapper.Insert<int>("[dbo].[SP_Add_Article]"
+                , dbparams,
+                commandType: CommandType.StoredProcedure));
+            return result;
+        }
+        [HttpGet(nameof(GetById))]
+        public async Task<Quote_Contact> GetById(int Id)
+        {
+            var result = await Task.FromResult(_dapper.Get<Quote_Contact>($"Select * from Quote_contact where Id = {Id}", null, commandType: CommandType.Text));
+            return result;
+        }
+        [HttpDelete(nameof(Delete))]
+        public async Task<int> Delete(int Id)
+        {
+            var result = await Task.FromResult(_dapper.Execute($"Delete Quote_contact Where Id = {Id}", null, commandType: CommandType.Text));
+            return result;
+        }
+        [HttpGet(nameof(Count))]
+        public Task<int> Count(int num)
+        {
+            var totalcount = Task.FromResult(_dapper.Get<int>($"select COUNT(*) from Quote_contact WHERE Age like '%{num}%'", null,
+                    commandType: CommandType.Text));
+            return totalcount;
+        }
+        [HttpPatch(nameof(Update))]
+        public Task<int> Update(Quote_Contact data)
+        {
+            var dbPara = new DynamicParameters();
+            dbPara.Add("Id", data.Id);
+            dbPara.Add("Name", data.Name, DbType.String);
+
+            var updateArticle = Task.FromResult(_dapper.Update<int>("[dbo].[SP_Update_Article]",
+                            dbPara,
+                            commandType: CommandType.StoredProcedure));
+            return updateArticle;
+        }
+    }
+}
+
---
+++ IPhoneRepairAPI/Models/Quote_Contact.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IPhoneRepairAPI.Models
+{
+    public class Quote_Contact
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public string Query { get; set; }
+        public string Location { get; set; }
+        public string Repair_type { get; set; }    
+
+    }
+}
